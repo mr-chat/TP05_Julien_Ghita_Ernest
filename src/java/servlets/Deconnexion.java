d37fda50f2e5b815b1eedf555fb02d3806e823a9
ernest <kolingba.ernest@gmail.com>
2017-05-22 16:28:28 +0200

    Servlets

---
+++ src/java/servlets/Deconnexion.java
@@ -0,0 +1,24 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class Deconnexion
+        extends HttpServlet {
+
+    public static final String URL_REDIRECTION = "/connexion";
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        /* Récupération et destruction de la sess
+ion en cours */
+        HttpSession session = request.getSession();
+        session.invalidate();
+        /* Redirection vers le formulaire de connexion */
+
+        response.sendRedirect(getServletContext().getContextPath() + URL_REDIRECTION);
+    }
+}
---
+++ src/java/servlets/Inscription.java
@@ -0,0 +1,50 @@
+package servlets;
+
+import beans.Utilisateur;
+import forms.InscriptionForm;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Inscription extends HttpServlet {
+
+    /* Des constantes */
+    private static final String ATT_FORM = "form";//form = message qu'on met dans setAttribute
+    private static final String ATT_USER = "utilisateur";//utilisateur = message que l'on met dans setAttribute
+    public static final String VUE = "/WEB-INF/inscription.jsp";
+
+    /* Requête GET sur notre formulaire => affichage du formulaire */
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        /* Affichage de la page d'inscription */
+        this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+    }
+
+    /* Soumission du formulaire => réception et contrôle des données */
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        /* Préparation de l'objet métier formulaire */
+        //on instancie l'objet metier
+        InscriptionForm form = new InscriptionForm();
+
+        /*
+         * Appel au traitement et à la validation de la requête, et récupération
+         * du bean en résultant
+         */
+        //inscrireUtilisateur est la methode d'InscriptionForm qui fait tout le boulot
+        Utilisateur utilisateur = form.inscrireUtilisateur(request);
+
+        /* Stockage du formulaire et du bean dans l'objet request */
+        request.setAttribute(ATT_FORM, form);
+        request.setAttribute(ATT_USER, utilisateur);
+
+        this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+    }
+
+}
